1446811c8b75a6c9463b9057cfb8452f6ae6110e
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-07 01:58:31 +0800

    Created a separate functional Home Screen Widget

---
+++ lib/Pages/get_started.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:groceryapp/pages/home_screen.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Annyeong!'),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Get Started!"),
+          onPressed: () {
+            //Navigate to Login but for now, Navigate to Home
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
