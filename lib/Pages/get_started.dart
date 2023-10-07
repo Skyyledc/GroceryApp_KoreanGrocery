d5315c8a91e4501d6defb3fdf09e9409ddb32be0
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-07 13:42:55 +0800

    Added a more friendly UI for Get Started

--- lib/Pages/get_started.dart
+++ lib/Pages/get_started.dart
@@ -1,22 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GetStarted extends StatelessWidget {
-  const GetStarted({super.key});
+  const GetStarted({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: const Text('Annyeong!'),
         backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
       body: Center(
-        child: ElevatedButton(
-          child: const Text("Get Started!"),
-          onPressed: () {
-            //Navigate to Login but for now, Navigate to Home
-            Navigator.pushReplacementNamed(context, '/landing');
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: const BorderRadius.all(Radius.circular(40)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[500]!,
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                    offset: const Offset(4.0, 4.0),
+                  ),
+                  const BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-4.0, -4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                  ),
+                ],
+              ),
+              child: const Icon(
+                Icons.food_bank_rounded,
+                size: 80,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30.0,
+                    vertical: 10.0,
+                  ),
+                  textStyle: const TextStyle(
+                    fontSize: 15.0,
+                  ),
+                ),
+                child: Text(
+                  "Get Started!",
+                  style: GoogleFonts.oxygen(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15,
+                  ),
+                ),
+                onPressed: () {
+                  // Navigate to Login but for now, Navigate to Home
+                  Navigator.pushReplacementNamed(context, '/landing');
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
