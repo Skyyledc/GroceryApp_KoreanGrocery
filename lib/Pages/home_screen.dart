90c0c296e7fedcd9c73459ed44f98fff6fc3f0c7
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-08 16:00:10 +0800

    Disabled Scrolling for Gridview

--- lib/Pages/home_screen.dart
+++ lib/Pages/home_screen.dart
@@ -314,54 +314,56 @@ class _HomeScreenState extends State<HomeScreen> {
             //Popular Items Text Container End
 
             //Popular Items Main Grid
-            GridView(
-              padding: EdgeInsets.zero,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
-                childAspectRatio: 1,
+            SingleChildScrollView(
+              physics: const NeverScrollableScrollPhysics(),
+              child: GridView(
+                padding: EdgeInsets.zero,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                  childAspectRatio: 1,
+                ),
+                shrinkWrap: true,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(0),
+                    child: Image.network(
+                      'https://images.unsplash.com/photo-1575932444877-5106bee2a599?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxfHxrb3JlYW4lMjBjaGlja2VufGVufDB8fHx8MTY5NjQyMzk4N3ww&ixlib=rb-4.0.3&q=80&w=1080',
+                      width: 300,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(0),
+                    child: Image.network(
+                      'https://images.unsplash.com/photo-1639321905636-c1c0e71fb467?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw0fHxiaWJpbWJhcHxlbnwwfHx8fDE2OTY0MjQwMTZ8MA&ixlib=rb-4.0.3&q=80&w=1080',
+                      width: 300,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(0),
+                    child: Image.network(
+                      'https://images.unsplash.com/photo-1679581083578-94eae6e8d7a4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxfHx0dGVva2Jva2tpfGVufDB8fHx8MTY5NjQyNDA0NXww&ixlib=rb-4.0.3&q=80&w=1080',
+                      width: 300,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(0),
+                    child: Image.network(
+                      'https://images.unsplash.com/photo-1562749606-0a9eb5a8a0f3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxfHxzYW1neWV0YW5nfGVufDB8fHx8MTY5NjQyNDA4MXww&ixlib=rb-4.0.3&q=80&w=1080',
+                      width: 300,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
               ),
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(0),
-                  child: Image.network(
-                    'https://images.unsplash.com/photo-1575932444877-5106bee2a599?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxfHxrb3JlYW4lMjBjaGlja2VufGVufDB8fHx8MTY5NjQyMzk4N3ww&ixlib=rb-4.0.3&q=80&w=1080',
-                    width: 300,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(0),
-                  child: Image.network(
-                    'https://images.unsplash.com/photo-1639321905636-c1c0e71fb467?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw0fHxiaWJpbWJhcHxlbnwwfHx8fDE2OTY0MjQwMTZ8MA&ixlib=rb-4.0.3&q=80&w=1080',
-                    width: 300,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(0),
-                  child: Image.network(
-                    'https://images.unsplash.com/photo-1679581083578-94eae6e8d7a4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxfHx0dGVva2Jva2tpfGVufDB8fHx8MTY5NjQyNDA0NXww&ixlib=rb-4.0.3&q=80&w=1080',
-                    width: 300,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(0),
-                  child: Image.network(
-                    'https://images.unsplash.com/photo-1562749606-0a9eb5a8a0f3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxfHxzYW1neWV0YW5nfGVufDB8fHx8MTY5NjQyNDA4MXww&ixlib=rb-4.0.3&q=80&w=1080',
-                    width: 300,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
             ),
 
             //Populer Items Main Grid End
