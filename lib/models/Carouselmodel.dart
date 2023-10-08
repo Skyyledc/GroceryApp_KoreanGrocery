04d3557a16b4978afc82821cb6e229ea816dfba6
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-08 18:01:46 +0800

    Refractored CarouselModel to be called easily

--- lib/models/Carouselmodel.dart
+++ lib/models/Carouselmodel.dart
@@ -1,24 +1,24 @@
-class DataModel {
+class CarouselModel {
   final String imageName;
   final String promoMsg;
 
-  DataModel(
+  CarouselModel(
     this.imageName,
     this.promoMsg,
   );
 }
 
-List<DataModel> dataList = [
-  DataModel(
+List<CarouselModel> dataList = [
+  CarouselModel(
       "https://images.unsplash.com/photo-1573806439793-82aa612294b2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxNXx8a29yZWFuJTIwZm9vZHxlbnwwfHx8fDE2OTY0MTg4OTN8MA&ixlib=rb-4.0.3&q=80&w=1080",
       "Get 50% off on your first order! :)"),
-  DataModel(
+  CarouselModel(
       "https://images.unsplash.com/photo-1548150914-c9f19106dbf6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw5fHxrb3JlYW4lMjBmb29kfGVufDB8fHx8MTY5NjQxODg5M3ww&ixlib=rb-4.0.3&q=80&w=1080",
       "Get 25% off on your first order!"),
-  DataModel(
+  CarouselModel(
       "https://images.unsplash.com/photo-1531263539449-56fdf29dfc4d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw1fHxzb2p1fGVufDB8fHx8MTY5NjM5MjA1OHww&ixlib=rb-4.0.3&q=80&w=1080",
       "Soju want to go out with your crush?"),
-  DataModel(
+  CarouselModel(
       "https://images.unsplash.com/photo-1532347231146-80afc9e3df2b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw1fHxrb3JlYW4lMjBmb29kfGVufDB8fHx8MTY5NjQxODg5M3ww&ixlib=rb-4.0.3&q=80&w=1080",
       "Free delivery on your first order!"),
 ];
