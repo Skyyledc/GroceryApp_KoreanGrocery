13e2c89d474bf3d0c252575f4863e22c2e6ad0d7
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-09 14:30:22 +0800

    Updated Home Screen

--- lib/Pages/home_screen.dart
+++ lib/Pages/home_screen.dart
@@ -37,7 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[100],
+      backgroundColor: const Color.fromARGB(255, 248, 155, 188),
       //Body Container
       body: SafeArea(
         child: SingleChildScrollView(
@@ -45,179 +45,176 @@ class _HomeScreenState extends State<HomeScreen> {
 //Main App Content
             children: [
               //Top Container for Choices
-              Align(
-                alignment: const AlignmentDirectional(0.00, 0.00),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        //First Child Container Fresh Meat
-                        Padding(
-                          padding:
-                              const EdgeInsetsDirectional.fromSTEB(5, 5, 5, 0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Container(
-                                width: 75,
-                                height: 75,
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      //First Child Container Fresh Meat
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(5, 5, 5, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Container(
+                              width: 75,
+                              height: 75,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Container(
+                                width: 120,
+                                height: 120,
+                                clipBehavior: Clip.antiAlias,
                                 decoration: const BoxDecoration(
-                                  color: Colors.white,
                                   shape: BoxShape.circle,
                                 ),
-                                child: Container(
-                                  width: 120,
-                                  height: 120,
-                                  clipBehavior: Clip.antiAlias,
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: Image.network(
-                                    'https://images.unsplash.com/photo-1607623814075-e51df1bdc82f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw0fHxtZWF0fGVufDB8fHx8MTY5NjM5MjA5MXww&ixlib=rb-4.0.3&q=80&w=1080',
-                                    fit: BoxFit.cover,
-                                  ),
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1607623814075-e51df1bdc82f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw0fHxtZWF0fGVufDB8fHx8MTY5NjM5MjA5MXww&ixlib=rb-4.0.3&q=80&w=1080',
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                              Text(
-                                'Fresh Meat',
-                                style: GoogleFonts.poppins(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 12,
-                                ), // Style
-                              ),
-                            ],
-                          ),
+                            ),
+                            Text(
+                              'Fresh Meat',
+                              style: GoogleFonts.poppins(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                              ), // Style
+                            ),
+                          ],
                         ),
-                        //First Child Container Fresh Meat End
+                      ),
+                      //First Child Container Fresh Meat End
 
-                        //Second Child Container Sauces
-                        Padding(
-                          padding:
-                              const EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Container(
-                                width: 75,
-                                height: 75,
+                      //Second Child Container Sauces
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Container(
+                              width: 75,
+                              height: 75,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Container(
+                                width: 120,
+                                height: 120,
+                                clipBehavior: Clip.antiAlias,
                                 decoration: const BoxDecoration(
-                                  color: Colors.white,
                                   shape: BoxShape.circle,
                                 ),
-                                child: Container(
-                                  width: 120,
-                                  height: 120,
-                                  clipBehavior: Clip.antiAlias,
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: Image.network(
-                                    'https://images.unsplash.com/photo-1472476443507-c7a5948772fc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwyfHxzYXVjZXxlbnwwfHx8fDE2OTYzOTIwNzZ8MA&ixlib=rb-4.0.3&q=80&w=1080',
-                                    fit: BoxFit.cover,
-                                  ),
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1472476443507-c7a5948772fc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwyfHxzYXVjZXxlbnwwfHx8fDE2OTYzOTIwNzZ8MA&ixlib=rb-4.0.3&q=80&w=1080',
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                              Text(
-                                'Sauces',
-                                style: GoogleFonts.poppins(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 12,
-                                ), // Style
-                              ),
-                            ],
-                          ),
+                            ),
+                            Text(
+                              'Sauces',
+                              style: GoogleFonts.poppins(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                              ), // Style
+                            ),
+                          ],
                         ),
-                        //Second Child Container Sauces End
+                      ),
+                      //Second Child Container Sauces End
 
-                        //Second Child Container Sauces
-                        Padding(
-                          padding:
-                              const EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Container(
-                                width: 75,
-                                height: 75,
+                      //Second Child Container Sauces
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Container(
+                              width: 75,
+                              height: 75,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Container(
+                                width: 120,
+                                height: 120,
+                                clipBehavior: Clip.antiAlias,
                                 decoration: const BoxDecoration(
-                                  color: Colors.white,
                                   shape: BoxShape.circle,
                                 ),
-                                child: Container(
-                                  width: 120,
-                                  height: 120,
-                                  clipBehavior: Clip.antiAlias,
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: Image.network(
-                                    'https://images.unsplash.com/photo-1569718212165-3a8278d5f624?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwzfHxyYW1lbnxlbnwwfHx8fDE2OTYzOTIwMjl8MA&ixlib=rb-4.0.3&q=80&w=1080',
-                                    fit: BoxFit.cover,
-                                  ),
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1569718212165-3a8278d5f624?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwzfHxyYW1lbnxlbnwwfHx8fDE2OTYzOTIwMjl8MA&ixlib=rb-4.0.3&q=80&w=1080',
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                              Text(
-                                'Ramen',
-                                style: GoogleFonts.poppins(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 12,
-                                ), // Style
-                              ),
-                            ],
-                          ),
+                            ),
+                            Text(
+                              'Ramen',
+                              style: GoogleFonts.poppins(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                              ), // Style
+                            ),
+                          ],
                         ),
-                        //Second Child Container Sauces End
+                      ),
+                      //Second Child Container Sauces End
 
-                        //Second Child Container Sauces
-                        Padding(
-                          padding:
-                              const EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Container(
-                                width: 75,
-                                height: 75,
+                      //Second Child Container Sauces
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Container(
+                              width: 75,
+                              height: 75,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Container(
+                                width: 120,
+                                height: 120,
+                                clipBehavior: Clip.antiAlias,
                                 decoration: const BoxDecoration(
-                                  color: Colors.white,
                                   shape: BoxShape.circle,
                                 ),
-                                child: Container(
-                                  width: 120,
-                                  height: 120,
-                                  clipBehavior: Clip.antiAlias,
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: Image.network(
-                                    'https://images.unsplash.com/photo-1562749606-0a9eb5a8a0f3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxfHxzYW1neWV0YW5nfGVufDB8fHx8MTY5NjQyNDA4MXww&ixlib=rb-4.0.3&q=80&w=1080',
-                                    fit: BoxFit.cover,
-                                  ),
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1562749606-0a9eb5a8a0f3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxfHxzYW1neWV0YW5nfGVufDB8fHx8MTY5NjQyNDA4MXww&ixlib=rb-4.0.3&q=80&w=1080',
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                              Text(
-                                'Beverages',
-                                style: GoogleFonts.poppins(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 12,
-                                ), // Style
-                              ),
-                            ],
-                          ),
+                            ),
+                            Text(
+                              'Beverages',
+                              style: GoogleFonts.poppins(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                              ), // Style
+                            ),
+                          ],
                         ),
-                        //Fourth Child Container Sauces End
-                      ],
-                      //Top Container for Choices End
-                    ),
+                      ),
+                      //Fourth Child Container Sauces End
+                    ],
+                    //Top Container for Choices End
                   ),
                 ),
               ),
@@ -304,9 +301,13 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               //Search Container -> Button Redirect End
+
+//Sizedbox to create a space
               const SizedBox(
                 height: 15,
               ),
+//Sizedbox to create a space end
+
               Container(
                 decoration: const BoxDecoration(
                   color: Colors.white,
