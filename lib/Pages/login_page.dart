403d1a26aaa863bab964d19b1a2af5200eb38def
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-09 22:28:50 +0800

    Update login_page.dart

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -9,11 +9,13 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _passwordVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.black,
+        color: Colors.white,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -21,41 +23,153 @@ class _LoginScreenState extends State<LoginScreen> {
               Text(
                 'Login',
                 style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontSize: 30,
+                  color: Colors.black,
+                  fontSize: 55,
                 ),
               ),
               Text(
-                'Login to account',
+                'Sign in to Continue',
                 style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 13,
                 ),
               ),
               const SizedBox(
                 height: 20,
               ),
-              // ignore: sized_box_for_whitespace
-              Container(
-                width: 350,
-                height: 150,
-                child: TextField(
+
+              //Email Text Field
+
+              SizedBox(
+                width: 300,
+                height: 50,
+                child: TextFormField(
                   cursorColor: Colors.black,
                   style: GoogleFonts.poppins(
                     color: Colors.black,
-                    fontSize: 18,
+                    fontSize: 14,
                   ),
                   decoration: InputDecoration(
                     filled: true,
-                    fillColor: Colors.white,
+                    fillColor: Colors.white12,
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                       borderSide: const BorderSide(
-                        color: Colors.white,
+                        color: Colors.black,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      borderSide: const BorderSide(
+                        color: Colors.black,
                       ),
                     ),
                     labelText: 'Email',
+                    labelStyle: GoogleFonts.poppins(
+                      fontSize: 14,
+                      color: Colors.black,
+                    ),
                   ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              //Password Text Field
+              SizedBox(
+                width: 300,
+                height: 50,
+                child: TextFormField(
+                  cursorColor: Colors.grey[100],
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                  obscureText: !_passwordVisible, // Password visibility toggle
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white12,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      borderSide: const BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
+                    labelText: 'Password',
+                    labelStyle: GoogleFonts.poppins(
+                      fontSize: 14,
+                      color:
+                          Colors.black, // Change label text color when focused
+                    ),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                      child: Icon(
+                        _passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.black,
+                        size: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              //Login Button
+              SizedBox(
+                width: 250,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink[300],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 15,
+              ),
+
+              Text(
+                'Forgot Password?',
+                style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 13,
+                ),
+              ),
+
+              Text(
+                'Signup!',
+                style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 13,
                 ),
               ),
             ],
