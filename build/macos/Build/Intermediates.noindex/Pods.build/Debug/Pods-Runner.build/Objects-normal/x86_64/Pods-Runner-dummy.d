fbc7b9114a6b5238c4222e01ec858ddddce18cd6
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-11 23:23:16 +0800

    Emulator Changes

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Steven/Documents/GitHub/GroceryApp_KoreanGrocery/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/Steven/Documents/GitHub/GroceryApp_KoreanGrocery/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Steven/Documents/GitHub/GroceryApp_KoreanGrocery/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
