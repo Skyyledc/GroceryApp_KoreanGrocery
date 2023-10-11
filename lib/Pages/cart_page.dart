22fdd0588e4e09469f626fbbaf04f86aac4fd4a2
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-11 18:36:45 +0800

    Added a cart page

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Cart Page"),
-      ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.pink[200],
+                  borderRadius:
+                      const BorderRadius.vertical(bottom: Radius.circular(18)),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Grocery Branch',
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 25,
+                      ),
+                    ),
+                    Text(
+                      'Address of branch',
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
