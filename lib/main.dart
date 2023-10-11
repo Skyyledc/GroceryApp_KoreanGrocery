b8f6818a5280828de2a58236d4a89e68450097fb
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-11 20:37:50 +0800

    Updated main from void to future to initialize app

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,11 @@ import 'package:groceryapp/pages/landing.dart';
 import 'package:groceryapp/pages/profile_page.dart';
 import 'package:groceryapp/pages/search_page.dart';
 import 'package:groceryapp/pages/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
