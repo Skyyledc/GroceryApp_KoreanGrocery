a0456180327ccc5048baeb090ce60b623ae81067
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-11 20:32:07 +0800

    Emulator Changes + Android Firebase implementation

--- build/app/generated/source/buildConfig/debug/com/example/groceryapp/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/groceryapp/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.racks.kgroceryapp;
+package com.example.groceryapp;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.racks.kgroceryapp";
+  public static final String APPLICATION_ID = "com.example.groceryapp";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
