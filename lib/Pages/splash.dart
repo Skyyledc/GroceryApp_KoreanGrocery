fb0c5ff1e2e14847f09d6a80a231cfdb65b805d9
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-11 20:37:16 +0800

    Updated splash conditions for redirecting

--- lib/Pages/splash.dart
+++ lib/Pages/splash.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:groceryapp/pages/get_started.dart';
+import 'package:groceryapp/pages/home_screen.dart'; // Change 'home.dart' to your actual home screen
 import 'package:lottie/lottie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -12,18 +12,49 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  bool isFirstTime = true;
+
   @override
   void initState() {
-    Timer(const Duration(seconds: 5), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const GetStarted(),
-        ),
-      );
-    });
-
     super.initState();
+    // Use an async function to await the result of checkFirstTime
+    _initialize();
+  }
+
+  Future<void> _initialize() async {
+    // Check if it's the first time using the app
+    bool isFirstTime = await checkFirstTime();
+    if (isFirstTime) {
+      // If it's the first time, navigate to the GetStarted screen
+      navigateToGetStarted();
+    } else {
+      // If not the first time, navigate to another screen (e.g., Home)
+      navigateToHome();
+    }
+  }
+
+  Future<bool> checkFirstTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('isFirstTime') ?? true;
+  }
+
+  void navigateToGetStarted() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const GetStarted(),
+      ),
+    );
+  }
+
+  void navigateToHome() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>
+            const HomeScreen(), // Change to your actual home screen
+      ),
+    );
   }
 
   @override
@@ -32,8 +63,7 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Center(
         child: Lottie.network(
           'https://lottie.host/f23731df-4606-4e22-85e5-e7bbfffab645/KozGbVTItV.json',
-          key:
-              const Key('lottie_animation'), // Add a key for the Lottie widget.
+          key: const Key('lottie_animation'),
         ),
       ),
     );
