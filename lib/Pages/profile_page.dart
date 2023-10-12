460972757c8efbe7d304ea8e72b022a1f348b3c3
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-13 00:14:16 +0800

    Added sign out button for testing

--- lib/Pages/profile_page.dart
+++ lib/Pages/profile_page.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:groceryapp/pages/login_page.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _signOut() async {
+    await _auth.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Profile Page"),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Profile Page'),
+            ElevatedButton(
+              onPressed: _signOut,
+              child: const Text('Sign Out'),
+            ),
+          ],
+        ),
       ),
     );
   }
