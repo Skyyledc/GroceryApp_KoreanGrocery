73b52b4f6cd9c01ea6ad1fdf4729ba362ab4b7bc
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-17 23:21:20 +0800

    Made a standalone bottom nav bar

---
+++ lib/utils/bottomNavBar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:groceryapp/pages/cart_page.dart';
+import 'package:groceryapp/pages/home/home_screen.dart';
+import 'package:groceryapp/pages/profile_page.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBar();
+}
+
+class _BottomNavBar extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = 0;
+  }
+
+  void _navigateBottomNav(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const HomeScreen(),
+    const CartPage(),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomNav,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ]);
+  }
+}
