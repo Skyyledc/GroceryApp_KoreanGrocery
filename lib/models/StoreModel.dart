46d89bb38bcc6153150d76e3aaf309a367e1de0d
Skyyledc <qskgdecastro@tip.edu.ph>
2023-10-19 23:31:57 +0800

    Initialized an id, rating, and distance for store model

--- lib/models/StoreModel.dart
+++ lib/models/StoreModel.dart
@@ -1,9 +1,22 @@
 class StoreInfo {
+  final String storeId;
   final String storeName;
   final String imageUrl;
+  final double storeRating;
+  final double storeDistance;
 
   StoreInfo({
+    required this.storeId,
     required this.storeName,
     required this.imageUrl,
+    required this.storeRating,
+    required this.storeDistance,
   });
+
+  StoreInfo.empty()
+      : storeId = '',
+        storeName = '',
+        imageUrl = '',
+        storeRating = 0.0,
+        storeDistance = 0.0;
 }
