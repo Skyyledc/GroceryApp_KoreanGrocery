44bb83eddbc796a2348ec410e18c343ef52aaf73
Skyyledc <qskgdecastro@tip.edu.ph>
2023-11-28 21:13:05 +0800

    Updated Landing to accomodate Cart

--- lib/Pages/landing.dart
+++ lib/Pages/landing.dart
@@ -9,6 +9,9 @@ class Landing extends StatefulWidget {
 
   @override
   State<Landing> createState() => _LandingState();
+
+  static _LandingState? of(BuildContext context) =>
+      context.findAncestorStateOfType<_LandingState>();
 }
 
 class _LandingState extends State<Landing> {
@@ -26,44 +29,55 @@ class _LandingState extends State<Landing> {
     });
   }
 
+  // Method to navigate to the Cart page
+  void navigateToCart() {
+    setState(() {
+      _selectedIndex = 1; // Set the index to the Cart page
+    });
+  }
+
   final List<Widget> _pages = [
     const HomeScreen(),
-    const CartPage(),
+    CartPage(),
     const ProfilePage(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: () async {
-          // Disable the back button when on the Landing page
-          return false;
-        },
-        child: Scaffold(
-          body: NestedScrollView(
-            headerSliverBuilder: (context, innerBoxIsScrolled) {
-              return [];
-            },
-            body: _pages[_selectedIndex],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-              currentIndex: _selectedIndex,
-              onTap: _navigateBottomNav,
-              type: BottomNavigationBarType.fixed,
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.shopping_cart),
-                  label: 'Cart',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  label: 'Profile',
-                ),
-              ]),
-        ));
+      onWillPop: () async {
+        // Disable the back button when on the Landing page
+        return false;
+      },
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [];
+          },
+          body: _pages[_selectedIndex],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _navigateBottomNav,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.pink.shade200,
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
